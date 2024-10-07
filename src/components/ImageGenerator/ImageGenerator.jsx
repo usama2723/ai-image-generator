cd8cd87fea2d56a8de36320bcb4bfdacd20949d9
usama2723 <sammirza201@gmail.com>
2024-10-07 17:43:36 +0500

    added input field

--- src/components/ImageGenerator/ImageGenerator.jsx
+++ src/components/ImageGenerator/ImageGenerator.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './ImageGenerator.css'
 import image from '../assests/image.jpg'
 const ImageGenerator = () => {
+
+    
     return (
         <div className='ai-image-generator'>
             <div className='header'>Ai Image Generator</div>
@@ -9,6 +11,10 @@ const ImageGenerator = () => {
                 <div className='image'>
                     <img height={'310px'} width={'310px'} src={image} alt='' />
                 </div>
+            </div>
+            <div className="search-box">
+                <input type='text' className='search-input' placeholder='Describe What You Want to See'></input>
+                <div className="generate-btn">Generate</div>
             </div>
         </div>
     )
